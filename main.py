d4347c322d75857c8024bc4e6a76a777dca17015
Roman Gerloff <71276897+RomanGoEmpire@users.noreply.github.com>
2024-01-21 21:08:44 +0100

    Refactor pixel position calculation and prime number generation

--- main.py
+++ main.py
@@ -1,38 +1,61 @@
 import os
 import sys
+import timeit
+import time
 
 os.environ["PYGAME_HIDE_SUPPORT_PROMPT"] = "1"
 import pygame
 
-pixel_size = 10
-half_pixel_size = pixel_size // 2
+size = 1000
+pixel_size = 2
 edges = []
-direction = "right"
+primes = []
+pixel_positions = []
 
 
-def get_next_pixel():
-    global x, y, direction, edges
+# rechts x+
+# links x-
+# runter y+
+# hoch y-
+
+
+def get_next_pixel(x, y, direction, edges):
     if direction == "right":
         x += pixel_size
         if x > edges[1]:
-            direction = "down"
+            direction = "up"
             edges[1] = x
     elif direction == "left":
         x -= pixel_size
         if x < edges[0]:
-            direction = "up"
+            direction = "down"
             edges[0] = x
-    elif direction == "up":
-        y -= pixel_size
-        if y < edges[2]:
-            direction = "right"
-            edges[2] = y
     elif direction == "down":
         y += pixel_size
         if y > edges[3]:
-            direction = "left"
+            direction = "right"
             edges[3] = y
-    return x, y
+    elif direction == "up":
+        y -= pixel_size
+        if y < edges[2]:
+            direction = "left"
+            edges[2] = y
+    return (x, y), direction, edges
+
+
+def fill_pixel_positions():
+    direction = "right"
+    x, y = size // 2, size // 2
+    edges = [x, x, y, y]
+    total_pixels = size * size // (pixel_size * pixel_size)
+    global pixel_positions
+    pixel_positions = [(x, y)]
+    start = timeit.default_timer()
+    for i in range(total_pixels):
+        next_pixel, direction, edges = get_next_pixel(x, y, direction, edges)
+        pixel_positions.append(next_pixel)
+        x, y = next_pixel
+    print(f"Time taken to fill pixel positions: {timeit.default_timer() - start}")
 
 
 def is_prime(n):
@@ -40,27 +63,28 @@ def is_prime(n):
         return True
     if n % 2 == 0:
         return False
-    if n < 2:
-        return False
-    for i in range(3, int(n**0.5) + 1, 2):
+    for i in range(3, int(n**0.5) + 1, 2):  # only odd numbers
         if n % i == 0:
             return False
     return True
+
+
+def fill_primes():
+    global size, primes
+    max_number = size * size
+    start = timeit.default_timer()
+    primes = [i for i in range(2, max_number - 1) if is_prime(i)]
+    print(f"Time taken to fill primes: {timeit.default_timer() - start}")
 
 
 # Initialize Pygame
 pygame.init()
 
 # Set the dimensions of the window
-screen_width = 400
-screen_height = 400
-screen = pygame.display.set_mode((screen_width, screen_height))
+screen = pygame.display.set_mode((size, size))
 
-# Set the starting position
-x = screen_width // 2
-y = screen_height // 2
-
-edges = [x, x, y, y]
+fill_primes()
+fill_pixel_positions()
 
 # Set the color
 red = (255, 0, 0)
@@ -68,14 +92,7 @@ green = (0, 255, 0)
 
 screen.fill((0, 0, 0))
 pygame.draw.rect(
-    screen,
-    green,
-    (
-        x,
-        y,
-        pixel_size,
-        pixel_size,
-    ),
+    screen, red, (pixel_positions[0][0], pixel_positions[0][1], pixel_size, pixel_size)
 )
 pygame.display.update()
 
@@ -83,36 +100,21 @@ pygame.display.update()
 # Keep the window open until it is closed
 
 done = False
-n = 2
 
+index = 0
 while True:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             sys.exit()
+
+    index_positions = primes[index]
     if not done:
-        next_pixel = get_next_pixel()
-        if (
-            next_pixel[0] < 0
-            or next_pixel[0] > screen_width
-            or next_pixel[1] < 0
-            or next_pixel[1] > screen_height
-        ):
+        if index_positions >= len(pixel_positions):
             done = True
-            print("Done!")
+            print("Done")
         else:
-            is_prime_number = is_prime(n)
-            if is_prime_number:
-                print(n, next_pixel)
-                pygame.draw.rect(
-                    screen,
-                    red,
-                    (
-                        next_pixel[0],
-                        next_pixel[1],
-                        pixel_size,
-                        pixel_size,
-                    ),
-                )
-                pygame.display.update()
-            n += 1
+            x, y = pixel_positions[index_positions - 1]
+            pygame.draw.rect(screen, green, (x, y, pixel_size, pixel_size))
+            pygame.display.update()
+            index += 1
